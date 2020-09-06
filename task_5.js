d5b9a45fc1852855a19eb079629adc5ad83a4857
Temiloluwa-ade <moboluwaji003@gmail.com>
2020-09-06 01:43:33 +0100

    Task 5

---
+++ task_5.js
@@ -0,0 +1,4 @@
+/*task_5 by temi*/
+var e="joel is a good boy";//this is the test string
+alert(e.replace(/[ ]/g,"%20"))//the method replace has two arguments the first for what you are replacing and the second for what you are replacing with, the first argument or parameter can take in a regular expression which makes it easier
+//the g signifies a global match, which means it searches through the whole string
